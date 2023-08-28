f178ca904f1bb91fb7d14ce673543061d7e365d4
Johnny Perdomo <johnnyperdomo7@gmail.com>
2023-08-27 21:52:19 -0400

    init react form, add payment or expense

--- src/pages/Transaction-Edit.tsx
+++ src/pages/Transaction-Edit.tsx
@@ -1,3 +1,77 @@
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import useTransactionStore from "../state/store";
+import { Transaction } from "../types/Transaction";
+import { TransactionType } from "../types/TransactionType";
+
+type Inputs = {
+    itemName: string;
+    price: number;
+};
+
 export function TransactionEdit() {
-    return <>Transaction Edit</>;
+    const [transactionType, setTransactionType] = useState<TransactionType>("expense");
+    const store = useTransactionStore();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        const newTransaction: Transaction = {
+            id: crypto.randomUUID(),
+            itemName: data.itemName,
+            price: data.price,
+            date: Date.now().toString(),
+            type: transactionType,
+        };
+
+        store.addTransaction(newTransaction);
+
+        console.log(store.transactions);
+
+        //TODO: route to newID
+    };
+
+    const handleToggleChange = (event: React.MouseEvent<HTMLElement>, newType: TransactionType) => {
+        setTransactionType(newType);
+    };
+
+    return (
+        <>
+            <h1>Overview</h1>
+
+            <h3>Select Transaction Type</h3>
+            <ToggleButtonGroup
+                color="primary"
+                value={transactionType}
+                exclusive
+                onChange={handleToggleChange}
+                aria-label="Platform">
+                <ToggleButton value="expense">Expense</ToggleButton>
+                <ToggleButton value="payment">Payment</ToggleButton>
+            </ToggleButtonGroup>
+
+            <br />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    {...register("itemName", { required: true })}
+                    type="text"
+                    placeholder="Gas"
+                />
+
+                <input
+                    {...register("price", {
+                        required: true,
+                    })}
+                    type="number"
+                    placeholder="123"
+                />
+                <input type="submit" />
+            </form>
+        </>
+    );
 }
